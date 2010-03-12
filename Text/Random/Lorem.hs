8d3d36e80a40e3cd8f6e8922043848e3c6d27e0b
terrence brannon <metaperl@lifehost.launchmodem.com>
2010-03-11 19:46:58 -0500

    stuck on rand num gen

--- Text/Random/Lorem.hs
+++ Text/Random/Lorem.hs
@@ -1,2 +1,27 @@
 {-- snippet module --}
 module Text.Random.Lorem  where
+
+import Data.Char
+import System.Random
+
+
+import System.Random hiding (next)
+
+
+nth n xs = head $ drop (n-1) xs
+
+lastIndex xs = (length xs) - 1
+
+randomList r = do g <- getStdGen
+return $ randomRs r g
+
+                  
+
+
+text = "Alias consequatur aut perferendis sit voluptatem accusantium doloremque aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis Nemo enim ipsam voluptatem quia voluptas sit suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae  et iusto odio dignissimos ducimus qui blanditiis praesentium laudantium, totam rem voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, Sed ut perspiciatis unde omnis iste natus error similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo porro quisquam est, qui minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores doloribus asperiores repellat."
+
+clean :: String -> String
+clean s = map toLower $ filter (\c -> isAlpha c || isSpace c) s
+
+sentenceJoiner = "  "  -- 2 spaces
+paragraphJoiner = "\n\n"
